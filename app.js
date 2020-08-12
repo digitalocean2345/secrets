0568863eb745d3cb1e8141d92b793e86814ffab8
digitalocean2345 <digitalocean2345@protonmail.com>
2020-08-12 21:52:55 +0530

    level 3 hashing

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 const encrypt = require("mongoose-encryption");
+const md5 = require('md5');
 
 
 const app = express();
@@ -24,8 +25,8 @@ const userSchema = new mongoose.Schema({
   email: String,
   password: String
 });
-const secret = process.env.SECRET;
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ['password'] });
+// const secret = process.env.SECRET;
+// userSchema.plugin(encrypt, { secret: secret, encryptedFields: ['password'] });
 
 
 const User = mongoose.model("User", userSchema);
@@ -45,7 +46,7 @@ app.get("/register", function(req, res){
 app.post("/register", function(req, res){
   const newUser = new User ({
     email: req.body.username,
-    password: req.body.password
+    password: md5(req.body.password)
   });
   newUser.save(function(err){
     if (err){
@@ -58,7 +59,8 @@ app.post("/register", function(req, res){
 
 app.post("/login", function(req, res){
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
+  console.log(password);
 
   User.findOne({email:username}, function(err,foundUser){
     if(err){
